467e73d472d1d3a6d2e52afb4775de1a1c872965
kavi pranesh <s.kavipranesh@outlook.com>
2024-07-08 15:16:39 +0530

    removed env mode in react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,9 +3,9 @@ import Navbar from './Components/Navbar'
 import TodoForm from './Components/TodoForm'
 import TodoList from './Components/TodoList'
 
-export const BASE_URL = import.meta.env.MODE == "development"   ?  "http://localhost:5000/api/todos/" : "/api/todos"
+//export const BASE_URL = import.meta.env.MODE == "development"   ?  "http://localhost:5000/api/todos/" : 
 
-
+export const BASE_URL = "/api/todos/"
 
 function App() {
   return (
